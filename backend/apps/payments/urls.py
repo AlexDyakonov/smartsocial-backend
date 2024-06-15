275288507b10da9e06ce990ba3f70ec0cd322661
Vaniog <forvantar@gmail.com>
2024-06-15 17:16:56 +0300

    merge fixes

--- backend/apps/payments/urls.py
+++ backend/apps/payments/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
 
-from .views import PaymentProcessingView, PaymentStatusView, yookassa_webhook, BookingsByOrderIdAPIView
 from .views import (
     PaymentProcessingView,
     PaymentStatusView,
     send_purchase_email_view,
     yookassa_webhook,
+    BookingsByOrderIdAPIView
 )
 
 urlpatterns = [
     path("yookassa-webhook/", yookassa_webhook, name="yookassa-webhook"),
     path("create/", PaymentProcessingView.as_view()),
     path("check/<str:payment_id>/", PaymentStatusView.as_view(), name="payment-status"),
-    path("order/<str:id>/bookings/", BookingsByOrderIdAPIView.as_view(), name="bookings-by-order")
+    path("order/<str:id>/bookings/", BookingsByOrderIdAPIView.as_view(), name="bookings-by-order"),
     path("test-mail/", send_purchase_email_view),
 ]
