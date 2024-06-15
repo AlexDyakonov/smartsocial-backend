f9d25bd415b59a20ad4c56b658a9bba20865183d
AlexDyakonov <shram.monolit@mail.ru>
2024-06-15 14:26:28 +0300

    Added email with attachment sending

--- backend/apps/payments/urls.py
+++ backend/apps/payments/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
-from .views import PaymentProcessingView, PaymentStatusView, yookassa_webhook
+from .views import (
+    PaymentProcessingView,
+    PaymentStatusView,
+    send_purchase_email_view,
+    yookassa_webhook,
+)
 
 urlpatterns = [
     path("yookassa-webhook/", yookassa_webhook, name="yookassa-webhook"),
     path("create/", PaymentProcessingView.as_view()),
     path("check/<str:payment_id>/", PaymentStatusView.as_view(), name="payment-status"),
+    path("test-mail/", send_purchase_email_view),
 ]
